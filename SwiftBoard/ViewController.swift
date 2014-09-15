444544c7a1c92761db61556be7823a2cb24250f0
Matt Daw <matt@mattdaw.com>
2014-09-15 08:35:29 -0700

    Initial Commit

---
+++ SwiftBoard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftBoard
+//
+//  Created by Matt Daw on 2014-09-15.
+//  Copyright (c) 2014 Matt Daw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
