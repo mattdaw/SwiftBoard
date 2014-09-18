33f8adbfb3d1957a2a9ec5c2bf7427a3da491537
Matt Daw <matt@mattdaw.com>
2014-09-18 13:24:33 -0700

    Force constraint evaluation in folder cell

---
+++ SwiftBoard/FolderCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FolderCollectionViewCell.swift
+//  SwiftBoard
+//
+//  Created by Matt Daw on 2014-09-18.
+//  Copyright (c) 2014 Matt Daw. All rights reserved.
+//
+
+import UIKit
+
+class FolderCollectionViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var containerView: UIView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        containerView.layer.cornerRadius = 5
+    }
+    
+    
+    override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes!) {
+        super.applyLayoutAttributes(layoutAttributes)
+        
+        // Trigger constraint re-evaluation, so the subview sizes get animated too
+        // http://stackoverflow.com/questions/23564453/uicollectionview-layout-transitions
+        layoutIfNeeded()
+    }
+}
