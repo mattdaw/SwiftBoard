bd79e937b90f4bbcd34cfdd31c992932a2c09e35
Matt Daw <matt@mattdaw.com>
2014-09-22 09:44:58 -0700

    WIP zooming

--- SwiftBoard/FolderCollectionViewCell.swift
+++ SwiftBoard/FolderCollectionViewCell.swift
@@ -22,6 +22,14 @@ class FolderCollectionViewCell : UICollectionViewCell {
     override func applyLayoutAttributes(layoutAttributes: UICollectionViewLayoutAttributes!) {
         super.applyLayoutAttributes(layoutAttributes)
         
+        if let flowLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout {
+            if bounds.width > 100 {
+                flowLayout.itemSize = CGSize(width: 96, height: 96)
+            } else {
+                flowLayout.itemSize = CGSize(width: 16, height: 16)
+            }
+        }
+        
         // Trigger constraint re-evaluation, so the subview sizes get animated too
         // http://stackoverflow.com/questions/23564453/uicollectionview-layout-transitions
         layoutIfNeeded()
