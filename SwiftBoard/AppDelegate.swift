cd2e126f214e376da8e06e42a9c63950591b6f6d
Matt Daw <matt@mattdaw.com>
2014-12-06 09:06:22 -0800

    Move view model init up to app delegate.

--- SwiftBoard/AppDelegate.swift
+++ SwiftBoard/AppDelegate.swift
@@ -16,6 +16,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        if let viewController = window?.rootViewController as? ViewController {
+            var viewModels: [ItemViewModel] = [
+                AppViewModel(name: "App 1", color: UIColor.greenColor()),
+                AppViewModel(name: "App 2", color: UIColor.blueColor()),
+                FolderViewModel(name: "Folder 1", viewModels: [
+                    AppViewModel(name: "App 5", color: UIColor.purpleColor()),
+                    AppViewModel(name: "App 6", color: UIColor.grayColor()),
+                    AppViewModel(name: "App 7", color: UIColor.yellowColor()),
+                    AppViewModel(name: "App 8", color: UIColor.yellowColor()),
+                    AppViewModel(name: "App 9", color: UIColor.redColor()),
+                    AppViewModel(name: "App 10", color: UIColor.purpleColor()),
+                    AppViewModel(name: "App 11", color: UIColor.blueColor()),
+                    ]),
+                FolderViewModel(name: "Folder 2", viewModels: [
+                    AppViewModel(name: "App 4", color: UIColor.darkGrayColor())
+                    ]),
+                AppViewModel(name: "App 3", color: UIColor.redColor()),
+                AppViewModel(name: "App 20", color: UIColor.redColor()),
+                AppViewModel(name: "App 21", color: UIColor.redColor()),
+                AppViewModel(name: "App 22", color: UIColor.redColor()),
+                AppViewModel(name: "App 23", color: UIColor.redColor()),
+                AppViewModel(name: "App 24", color: UIColor.redColor())
+            ]
+            
+            viewController.rootViewModel = RootViewModel(viewModels: viewModels)
+        }
+        
         return true
     }
 
--- SwiftBoard/ViewController.swift
+++ SwiftBoard/ViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var rootCollectionView: RootCollectionView!
-    private var rootViewModel: RootViewModel?
+    var rootViewModel: RootViewModel?
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent
@@ -20,36 +20,11 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // TODO: Move to AppDelegate and pass in
-        var viewModels: [ItemViewModel] = [
-            AppViewModel(name: "App 1", color: UIColor.greenColor()),
-            AppViewModel(name: "App 2", color: UIColor.blueColor()),
-            FolderViewModel(name: "Folder 1", viewModels: [
-                AppViewModel(name: "App 5", color: UIColor.purpleColor()),
-                AppViewModel(name: "App 6", color: UIColor.grayColor()),
-                AppViewModel(name: "App 7", color: UIColor.yellowColor()),
-                AppViewModel(name: "App 8", color: UIColor.yellowColor()),
-                AppViewModel(name: "App 9", color: UIColor.redColor()),
-                AppViewModel(name: "App 10", color: UIColor.purpleColor()),
-                AppViewModel(name: "App 11", color: UIColor.blueColor()),
-                ]),
-            FolderViewModel(name: "Folder 2", viewModels: [
-                AppViewModel(name: "App 4", color: UIColor.darkGrayColor())
-                ]),
-            AppViewModel(name: "App 3", color: UIColor.redColor()),
-            AppViewModel(name: "App 20", color: UIColor.redColor()),
-            AppViewModel(name: "App 21", color: UIColor.redColor()),
-            AppViewModel(name: "App 22", color: UIColor.redColor()),
-            AppViewModel(name: "App 23", color: UIColor.redColor()),
-            AppViewModel(name: "App 24", color: UIColor.redColor())
-        ]
-        
-        rootViewModel = RootViewModel(viewModels: viewModels)
         rootCollectionView.rootViewModel = rootViewModel
     }
     
     @IBAction func handleHomeButton(sender: UIButton) {
-        rootViewModel!.editingModeEnabled = false
+        rootViewModel?.editingModeEnabled = false
     }
 }
 
